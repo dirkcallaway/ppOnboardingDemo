04e6a3cd29606a07dc8caada62b10b5380f603eb
dirkcallaway <35826733+dirkcallaway@users.noreply.github.com>
2019-08-06 10:13:22 -0600

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <LoginTest />
+    <OnboardList />
+  </div>
+</template>
+
+<script>
+import OnboardList from './components/OnboardList.vue'
+import LoginTest from './components/LoginTest.vue'
+
+export default {
+  name: 'app',
+  components: {
+    OnboardList,
+    LoginTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
+<style src='./assets/style.css'></style>
+
